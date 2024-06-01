44bd1457f95b427e90d8ced7fcd45c6d21fa7a3c
priyapop <dangolpriya21@gmail.com>
2024-06-01 12:27:19 +0545

    Feat: Bloby game project created.

---
+++ Assets/pipespawnerscript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipespawnerscript : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnrate = 2;
+    private float timer = 0;
+    public float heightoffset = 10;
+    void Start()
+    {
+        spawnpipe();
+    }
+
+   
+    void Update()
+    {
+        if (timer < spawnrate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            spawnpipe();
+            timer = 0;
+        }
+    }
+    void spawnpipe()
+    {
+        float lowestpoint = transform.position.y - heightoffset;
+        float highestpoint = transform.position.y + heightoffset;
+        Instantiate(pipe, new Vector3(transform.position.x,Random.Range(lowestpoint,highestpoint),0), transform.rotation);
+    }
+}
